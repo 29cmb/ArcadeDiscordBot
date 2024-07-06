a769d90b24d31a32ad31fbe0c2e713a0aa70e4f7
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-06 10:54:01 -0400

    doesn't work, but I did something

---
+++ commands/arcade/stats.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stats')
+		.setDescription('Checks your stats')
+        .addStringOption(option => 
+            option
+            .setName("id")
+                .setDescription("Set the slack user ID to check")
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option
+                .setName("key")
+                .setDescription("Your hack club API key")
+                .setRequired(true) // TODO: Hook up to a database so you just have to run /setCredentials
+        ),
+	async execute(interaction) {
+		const slackID = interaction.options.getString("id")
+        const key = interaction.options.getString("key")
+        const url = `https://hackhour.hackclub.com/api/stats/${slackID}`
+        axios.get(url, {
+            headers: {
+                "Authorization": `Bearer ${key}`,
+                "User-Agent": "Arcade-Discord-Bot/0.1.0",
+                "Content-Tye": "application/json"
+            }
+        }).then(data => {
+            if(data.data.ok == true){
+                interaction.reply({ content: `Total Sessions: ${JSON.parse(data).data.sessions}\nTotal minutes: ${JSON.parse(data).data.total}`, ephemeral: true})
+            }
+        })
+	},
+};
