373b2eb3cf6858c8d0f3e3062c672a769ee137fa
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-06 17:47:16 -0400

    Added session starting, will open a thread, but nothing else. Slack messaging coming soon™️

--- commands/arcade/register.js
+++ commands/arcade/register.js
@@ -1,36 +1,36 @@
 const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('register')
-		.setDescription('Register your slack ID and api key'),
+                .setName('register')
+                .setDescription('Register your slack ID and api key'),
 	async execute(interaction) {
-        const modal = new ModalBuilder()
-        modal.setCustomId("credentialsInput")
-        modal.setTitle("Arcade Credentias")
+                const modal = new ModalBuilder()
+                modal.setCustomId("credentialsInput")
+                modal.setTitle("Arcade Credentias")
 
-        const slackID = new TextInputBuilder()
-        .setCustomId("slackID")
-        .setLabel("What is your slack ID?")
-        .setStyle(TextInputStyle.Short)
-        .setPlaceholder("\"#what-is-my-slack-id\" on the HackClub slack")
-        .setRequired(true);
+                const slackID = new TextInputBuilder()
+                .setCustomId("slackID")
+                .setLabel("What is your slack ID?")
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder("\"#what-is-my-slack-id\" on the HackClub slack")
+                .setRequired(true);
 
-        const apiKey = new TextInputBuilder()
-        .setCustomId("apiKey")
-        .setLabel("What is your hack club API key?")
-        .setStyle(TextInputStyle.Short)
-        .setPlaceholder("\"/api\" in the hackclub slack")
-        .setRequired(true);
+                const apiKey = new TextInputBuilder()
+                .setCustomId("apiKey")
+                .setLabel("What is your hack club API key?")
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder("\"/api\" in the hackclub slack")
+                .setRequired(true);
 
-        const confirmation = new TextInputBuilder()
-        .setCustomId("confirmation")
-        .setLabel("Type \"confirm\" to continue")
-        .setPlaceholder("All data is encrypted, bot source code is on github.")
-        .setStyle(TextInputStyle.Short)
-        .setRequired(true);
+                const confirmation = new TextInputBuilder()
+                .setCustomId("confirmation")
+                .setLabel("Type \"confirm\" to continue")
+                .setPlaceholder("All data is encrypted, bot source code is on github.")
+                .setStyle(TextInputStyle.Short)
+                .setRequired(true);
 
-        modal.addComponents(new ActionRowBuilder().addComponents(slackID), new ActionRowBuilder().addComponents(apiKey), new ActionRowBuilder().addComponents(confirmation))
+                modal.addComponents(new ActionRowBuilder().addComponents(slackID), new ActionRowBuilder().addComponents(apiKey), new ActionRowBuilder().addComponents(confirmation))
 
-        await interaction.showModal(modal)
+                await interaction.showModal(modal)
 	},
 };
---
+++ commands/arcade/start.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, Events, ComponentType, ThreadAutoArchiveDuration } = require('discord.js');
+const axios = require('axios')
+const db = require("../../modules/db.js")
+const encryption = require("../../modules/encryption.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('start')
+		.setDescription('Start a new session')
+        .addStringOption(option => 
+            option
+            .setName("work")
+                .setDescription("What are you working on?")
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+        await db.client.connect()
+        const data = await db.collections.credentials.findOne({userId: interaction.user.id})
+        await db.client.close()
+        
+		if(!data) return interaction.reply("You need to run /register first!")
+            
+
+        const slackID = encryption.decrypt(data.slackId)
+        const key = encryption.decrypt(data.apiKey)
+
+        const url = `https://hackhour.hackclub.com/api/start/${slackID}`
+
+        await axios.post(url, {
+            work: interaction.options.getString("work")
+        }, 
+        { 
+            headers: {
+                "Authorization": `Bearer ${key}`,
+                "User-Agent": "Arcade-Discord-Bot/0.1.0",
+                "Content-Tye": "application/json"
+            }
+        }).then(async response => {
+            if(response.data.ok){
+                interaction.reply("Session started!").then(async msg => {
+                    const thread = await msg.startThread({
+                        name: interaction.user.username,
+                        autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
+                        reason: `${interaction.user.username} finished their hour!`
+                    })
+                })
+            } else {
+                if(response.data.error == "You already have an active session"){
+                    interaction.reply("You already have an active session!")
+                }
+            }
+        }).catch(e => {
+            if(e.response){
+                if(e.response.data.error == "You already have an active session"){
+                    interaction.reply("You already have an active session!")
+                } else {
+                    interaction.reply({ content: `An unknown error occured when trying to reach the api. This is most likely a hack club outage.\n${e}`, ephemeral: true})
+                }
+            } else {
+                interaction.reply({ content: `An unknown error occured when trying to reach the api. This is most likely a hack club outage.\n${e}`, ephemeral: true})
+            }
+           
+        })
+	},
+};
