68645e33b8d6b5fa956cc016eab835e9c488be02
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-06 11:08:47 -0400

    Added proper error handling

--- commands/arcade/stats.js
+++ commands/arcade/stats.js
@@ -29,9 +29,27 @@ module.exports = {
             }
         }).then(response => {
             if(response.data.ok == true){
-                console.log(response.data)
                 interaction.reply({ content: `Total Sessions: ${response.data.data.sessions}\nTotal minutes: ${response.data.data.total}`, ephemeral: true})
+            } else {
+                if(response.data.error == "Unauthorized"){
+                    interaction.reply({ content: "Your API key is invalid", ephemeral: true })
+                } else {
+                    interaction.reply({ content: `The api returned an error: ${response.data.error}`})
+                }
             }
+        }).catch(e => {
+            if(e.response){
+                if(e.response.data.error == "Unauthorized"){
+                    interaction.reply({ content: "Your API key is invalid", ephemeral: true })
+                } else if(e.response.data.error == "User not found") {
+                    interaction.reply({ content: "User ID is invalid.", ephemeral: true})
+                } else {
+                    interaction.reply({ content: `The api returned an error: ${e.response.data.error}`, ephemeral: true})
+                }
+            } else {
+                interaction.reply({ content: `An unknown error occured when trying to reach the api. This is most likely a hack club outage.`, ephemeral: true})
+            }
+            
         })
 	},
 };
